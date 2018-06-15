cc250dc25f70e71211772cb5d69328707208e5bf
the42ndturtle <the42ndturtle@gmail.com>
2018-06-14 21:00:58 -0400

    api stuff

--- api/getArticles.php
+++ api/getArticles.php
@@ -1,7 +1,7 @@
 <?php
   include($_SERVER['DOCUMENT_ROOT']."/includes/include.php");
   if ($_SERVER['SERVER_ADDR'] != $_SERVER['REMOTE_ADDR']){
-    $this->output->set_status_header(400, 'No Remote Access Allowed');
+    http_response_code(400);
     exit;
   }
   $type = $_GET['type'];
