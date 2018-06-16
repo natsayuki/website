77de60b4f0997ef3760440d59c7d75e239c2b8d9
the42ndturtle <the42ndturtle@gmail.com>
2018-06-15 22:12:25 -0400

    mobile

--- script.js
+++ script.js
@@ -182,4 +182,9 @@ $(document).ready(function(){
       }
     }
   });
+  $(window).bind("orientationchange", function(){
+     var orientation = window.orientation;
+     var new_orientation = (orientation) ? 0 : 180 + orientation;
+     $('body').css({"-webkit-transform": "rotate(" + new_orientation + "deg)"});
+   });
 });
