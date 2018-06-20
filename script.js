4b9b96298557202779a3bacacf80735ac3959130
the42ndturtle <the42ndturtle@gmail.com>
2018-06-20 13:42:39 -0400

    reviews

--- script.js
+++ script.js
@@ -168,7 +168,8 @@ $(document).ready(function(){
     type: 'get',
     data: {type: 'new'},
     success: function(data){
-      data = data.split('').join('');
+      data = data.split('"?').join('');
+      data = data.split('?"').join('');
       console.log(data);
       data = JSON.parse(data);
       $.each(data.reverse(), function(key, value){
